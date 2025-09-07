a2da48d308499c654a9450e6a88d910f3740f107
Aashin A P <apaashin@gmail.com>
2025-09-07 10:52:23 +0530

    Basic Imports and Starting initialization for Vignere Cipher written

---
+++ Vignere Cipher/Server.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Server{
+    public static void main(String args[]) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        ServerSocket ss = new ServerSocket(4444);
+        System.out.println("Server is running at port 4444....");
+        Socket s = ss.accept();
+        System.out.println("Client Connected...");
+        PrintWriter pw = new PrintWriter(s.getOutputStream(),true);
+    }
+}
