4125ae4018942dff019bfbb7aa5381a57f1b9cc8
Aashin A P <apaashin@gmail.com>
2025-09-07 11:18:29 +0530

    Added Basic imports and initialisations in Hill Cipher

---
+++ Hill Cipher/Client.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Client{
+    public static void main(String args[]) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        Socket s = new Socket("localhost", 4444);
+        System.out.println("Connected to Server...");
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        String cipherText = br.readLine();
+        System.out.println("Encrypted text [Received]: " + cipherText);
+    }
+}
