4d41269c6957ead99115475ec10c49e325495c25
Aashin A P <apaashin@gmail.com>
2025-09-08 07:32:51 +0530

    Added rail fence decryption and removed encryption code

--- Rail Fence Cipher/CommonUtils.java
+++ Rail Fence Cipher/CommonUtils.java
@@ -1,35 +1,58 @@
 public class CommonUtils{
 
     public static String railfence_encrypt(String plainText, int key){
-        int n = plainText.length();
+        
+    }
+
+    public static String railfence_decrypt(String cipherText, int key){
+        int n = cipherText.length();
         char[][] rail = new char[key][n];
         for(int i = 0; i < key; i++){
             for(int j = 0; j < n; j++){
                 rail[i][j] = '\n';
             }
         }
-        boolean dir_down = false;
-        int row = 0, col = 0;
+
+        int row = 0;
+        int col = 0;
+        boolean dir_down = true;
 
         for(int i = 0; i < n; i++){
-            if(row == 0 || row == key - 1)
-                dir_down = !dir_down;
-            rail[row][col++] = plainText.charAt(i);
+            if (row == 0){
+                dir_down = true;
+            }
+            if (row == key-1){
+                dir_down = false;
+            }
+            rail[row][col++] = '*';
             row += dir_down ? 1 : -1;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < key; i++){
+        int index = 0;
+
+        for(int i = 0 ; i < key; i++){
             for(int j = 0; j < n; j++){
-                if (rail[i][j]!='\n'){
-                    sb.append(rail[i][j]);
+                if(rail[i][j] == '*' && index < n){
+                    rail[i][j] = cipherText.charAt(index++);
                 }
             }
         }
-        return sb.toString();
-    }
 
-    public static String railfence_decrypt(String cipherText, int key){
-        return railfence_encrypt(cipherText,-key);
+        StringBuilder sb = new StringBuilder();
+
+        row = 0; col = 0;
+
+        for(int i = 0; i < n; i++){
+            if (row == 0)
+                dir_down = true;
+            if (row == key-1)
+                dir_down = false;
+            
+            if (rail[row][col] != '\n')
+                sb.append(rail[row][col++]);
+            
+            row += dir_down ? 1:-1;
+        }
+        return sb.toString();
     }
 }
