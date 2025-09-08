2d4157613dd4c03450db922a673220924980ef59
Aashin A P <apaashin@gmail.com>
2025-09-08 07:44:59 +0530

    Added rail Fence encryption code

--- Rail Fence Cipher/CommonUtils.java
+++ Rail Fence Cipher/CommonUtils.java
@@ -33,6 +33,46 @@ public class CommonUtils{
     }
 
     public static String railfence_decrypt(String cipherText, int key){
-        
+        int n = cipherText.length();
+        char[][] rail = new char[key][n];
+        for(int i = 0; i < key; i++){
+            for(int j = 0; j < n; j++){
+                rail[i][j] = '\n';
+            }
+        }
+
+        boolean dir_down = true;
+        int row = 0, col = 0;
+
+        for(int i = 0; i < n; i++){
+            if (row == 0) dir_down = true;
+            if (row == key-1) dir_down = false;
+            rail[row][col++] = '*';
+            row += dir_down ? 1:-1;
+        }
+
+        int index = 0;
+        for(int i = 0; i < key; i++){
+            for(int j = 0; j < n; j++){
+                if(index < n && rail[i][j] == '*'){
+                    rail[i][j] = cipherText.charAt(index++);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        row = 0; col = 0;
+        dir_down = true;
+
+        for(int i = 0; i < n; i++){
+            if (row == 0) dir_down = true;
+            if (row == key - 1) dir_down = false;
+            if(rail[row][col] != '\n'){
+                sb.append(rail[row][col++]);
+            }
+            row += dir_down ? 1:-1;
+        }
+        return sb.toString();
     }
 }
